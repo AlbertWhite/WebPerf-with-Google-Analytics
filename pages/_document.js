97799adef4302b9025fb2fd9b99fdc1570271458
Albert XU <xyb1114@gmail.com>
2020-04-01 16:16:03 +0200

    fixup

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,9 @@ export default class extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
           `,
             }}
           />
--- utils/analytics.js
+++ utils/analytics.js
@@ -3,8 +3,15 @@
 export const GA_TRACKING_ID = 'UA-162274920-1'
 
 export const trackPageview = (url) => {
-  console.warn('ax', { url });
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
+  })
+}
+
+export const trackEvent = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
   })
 }
